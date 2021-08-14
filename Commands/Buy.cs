263ce0e7ed705d8882942a7e26116f33f6009747
Jack Bauerle <jackbauerle@Jacks-Mac-mini.hsd1.ma.comcast.net>
2021-08-14 16:22:42 -0400

    hello bae

---
+++ Commands/Buy.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LineCommander;
+
+namespace BaeTrader.Commands
+{
+    public class Buy : BaseCommand
+    {
+        public override string Description()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private const int MAX_INVESTMENTS = 10;
+        private const double MIN_PRICE_IN_DOLLARS = .5;
+
+
+        public override Task<bool> Execute(IEnumerable<string> arguments)
+        {
+            // check args for market
+            // check args for budget
+            // load market
+            // sort by price
+
+        }
+
+        public override IEnumerable<string> MatchingBaseCommands()
+        {
+            return new List<string>() { "buy" };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using LineCommander;
+
+namespace bae_trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new List<BaseCommand>() { };
+            var commander = new Commander();
+            commander.AddCommands(commands);
+            commander.ListenForCommands();
+        }
+    }
+}
