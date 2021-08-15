e0b3581a7d0cdcfa8fd0d612dcff31e4d0cd6772
Jack Bauerle <jackbauerle@Jacks-Mac-mini.hsd1.ma.comcast.net>
2021-08-14 22:26:37 -0400

    adding blank credentials

---
+++ Configuration/AlpacaCredentials.cs
@@ -0,0 +1,12 @@
+namespace bae_trader.Configuration
+{
+    public class AlpacaCredentials
+    {
+        public const string PaperClientID =  "";
+        public const string PaperClientSecret = "";
+
+        public const string LiveClientID =  "";
+        public const string LiveClientSecret = "";
+
+    }
+}
